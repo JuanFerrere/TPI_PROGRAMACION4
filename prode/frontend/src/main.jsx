4f975cd8926cd077eefb780b95b86a4b01da1d94
gererock <1rochi9gere@gmail.com>
2026-06-09 21:03:59 -0300

    Agrega frontend React con login y registro

---
+++ prode/frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "./App.jsx";
+import Registro from "./pages/Registro.jsx";
+import Login from "./pages/Login.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
